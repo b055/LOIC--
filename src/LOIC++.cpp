a76fe4fc8c53fdfba7bc0975642d102ca74d0b85
b055 <incadri@gmail.com>
2012-11-08 10:07:02 +0200

    Created the LOIC++

---
+++ src/LOIC++.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : LOIC++.cpp
+// Author      : Ivan Nelson Togboa Cadri
+// Version     :
+// Copyright   : 
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
